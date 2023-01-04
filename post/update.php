4c92efe07430920da3ea7ef7882ec777b96f0ae4
young0264 <ny2485@naver.com>
2023-01-04 18:04:06 +0900

    Add : 게시글 수정

--- post/update.php
+++ post/update.php
@@ -8,21 +8,19 @@
 
 <?php
     $postId=$_GET['postId'];
-//    echo"<h2>$postId</h2>";
-//    exit;
     $connect = mysqli_connect("localhost", "root", "7pifz9!!", "loginexam") or die("fail");
     $sql = "SELECT * FROM post where id = '$postId'";
     $result = mysqli_query($connect, $sql);
-//    $result = $connect->query($sql);
     $post=mysqli_fetch_assoc($result);
 
     ?>
 <div id="board_write">
-    <h4>글을 작성하는 공간입니다.</h4>
+    <h4>게시글 수정 </h4>
     <div id="write_area">
-        <form action="/post/update_ok.php" method="post">
+        <form action="/untitled/post/update_ok.php" method="post">
+<!--      value부분 단축용어, hidden으로 보내기     -->
             <input type="hidden" name="postId" value="<?=$postId?>">
-<!--        <form action="testPost.php?postId=--><?php //echo $postId; ?><!--" method="post">-->
+
             <div id="in_title">
                 <textarea name="title" id="utitle" rows="3" cols="40" placeholder="제목" maxlength="100" required>
                 <?php echo $post['title']; ?>
@@ -34,7 +32,6 @@
             <div id="in_content">
                 <textarea name="content" id="ucontent" rows="10" cols="40" placeholder="내용" required>
                 <?php echo $post['content']; ?>
-
                 </textarea>
             </div>
 
--- post/update_ok.php
+++ post/update_ok.php
@@ -1,21 +1,23 @@
 <?php
-$postId=$_GET['postId'];
-exit;
-
-echo "<h2>$postId</h2>";
 $connect = mysqli_connect("localhost", "root", "7pifz9!!", "loginexam") or die("fail");
 
-//    echo"<h2>$postId</h2>";
-//    exit;
-echo "<h2>$postId</h2>";
-exit;
+$postId=$_POST['postId'];
 $username = $_COOKIE['id'];
 $password = $_COOKIE['pw'];
 $title=$_POST['title'];
 $content=$_POST['content'];
-//$title = $_GET['title'];
-//$content = $_GET['content'];
-$sql = "update post set title='".$title."',content='".$content."' where id='".$postId."'" ?>
 
-<!--<script type="text/javascript">alert("수정되었습니다."); </script>-->
+echo "<h2>$postId, $username, $password, $title, $content</h2>";
+//$sql = "update post set title='".$title."',content='".$content."' where id='".$postId."'"
+
+$query = "update post set title='".$title."' , content='".$content."' where id=$postId";
+
+mysqli_query($connect, $query);
+mysqli_close($connect);
+?>
+
+"<script>
+    alert('수정이 완료되었습니다.');
+    location.replace("/untitled/post/list.php");
+</script>";
 <!--<meta http-equiv="refresh" content="0 url=/page/board/read.php?idx=--><?php //echo $postId; ?><!--">-->
