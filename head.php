22cf8ca143c305c278c2f164aee42ba743c224ff
young0264 <ny2485@naver.com>
2023-01-04 18:01:26 +0900

    Add: 쿠기,세션으로  로그인기능 구현

---
+++ head.php
@@ -0,0 +1,9 @@
+<?php
+?>
+<head>
+    <meta charset="UTF-8">
+    <title>Board</title>
+    <link rel="stylesheet" href="/untitled/css/style.css">
+</head>
+<?php
+?>
--- login_action.php
+++ login_action.php
@@ -1,8 +1,8 @@
 <?php
 
-$nickname=$_GET['nickname'];
-$password=$_GET['password'];
-$email =$_GET['email'];
+$nickname = $_GET['nickname'];
+$password = $_GET['password'];
+$email = $_GET['email'];
 
 $connect = mysqli_connect("localhost", "root", "7pifz9!!", "loginexam") or die("fail");
 
@@ -11,8 +11,7 @@ $signup = "insert into register (nickname, password, email) values ('$nickname',
 if ($connect->query($signup)) {
     echo "sign up success!!</br >";
     echo "<a href=mainpage.html>로그인 페이지로 이동</a> ";
-}
-else{
-    echo"fail to insert sql";
+} else {
+    echo "fail to insert sql";
 }
 ?>
