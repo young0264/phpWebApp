fb596bb66ff3df96c6a1f48e9cd20fa25022df32
young0264 <ny2485@naver.com>
2023-01-04 18:05:56 +0900

    Add: 게시글삭제

--- post/delete.php
+++ post/delete.php
@@ -1,1 +1,23 @@
 <?php
+$connect = mysqli_connect("localhost", "root", "7pifz9!!", "loginexam") or die("fail");
+
+$postId = $_POST['postId'];
+$query = "delete from post where id='$postId';";
+
+mysqli_query($connect, $query);
+
+$query = "select from post where id='$postId';";
+
+if (mysqli_query($connect, $query)) {
+    echo "<h2>값이 삭제되지 않았습니다</h2>";?>
+<?php
+}else{
+    ?>
+    <script type="text/javascript">
+        alert("게시글이 삭제되었습니다.");
+        location.replace("/untitled/post/list.php");
+    </script>
+<?php
+}
+mysqli_close($connect);
+?>
