28ecde83b77e1de73abd65702a74bb5c02bd4fcf
young0264 <ny2485@naver.com>
2023-01-04 18:04:36 +0900

    Add : 게시글 리스트페이지

--- post/list.php
+++ post/list.php
@@ -6,8 +6,12 @@
     <link rel="stylesheet" href="/untitled/css/style.css">
 </head>
 <body>
+<script type="text/javascript">
+    function delete_id(id) {
+        alert(id);
+    }
+</script>
 <div class=top><h2>게시판</h2></div>
-
 <button class="no" onclick="window.location.href='write.html'">글쓰기</button>
 <table class="middle">
     <thead>
@@ -21,28 +25,53 @@
     </tr>
     </thead>
     <div class="pagination">
-        <?php
-
-
+<?php
         $connect = mysqli_connect("localhost", "root", "7pifz9!!", "loginexam") or die("fail");
-
-        $sql = "SELECT * FROM post;";
+        $sql = "select * from post";
         $result = mysqli_query($connect, $sql);
+        $row_cnt = mysqli_fetch_array($result);
         $post_cnt = mysqli_num_rows($result);
         $page_cnt = ceil($post_cnt / 10);
-        ?>
-        <form action="pagingList.php" method="post">
-            <?php
-            for ($page = 1;$page <= $page_cnt; $page++) {
-            echo "<input type='submit' name='pageNum' value='$page'>";
-            //                echo "<div> "  .$page. " </div>";
+        $pageNum = !empty($_GET['pageNum']) ? $_GET['pageNum'] : 1;
 
+        $cnt = ($pageNum - 1) * 10;
+
+        $sql2 = "select * from post 
+                    order by id desc 
+                        limit $cnt, 10";
+
+        $res2 = mysqli_query($connect, $sql2);
+        while ($row = mysqli_fetch_array($res2)) {
+?>
+                <tbody>
+                <tr>
+                    <td><?=$row['id']?></td>
+                    <td>
+                        <a href="/untitled/post/update.php?postId=<?=$row['id']?>"><?php echo $row['title']; ?>
+                    </td>
+                    <td><?=$row['content']?></td>
+                    <td><?php echo $row['nickname']; ?></td>
+                    <td><?php echo $row['created']; ?></td>
+                    <td>
+                        <button type="button" onclick="delete_id('<?=$row['id']?>')">삭제</button>
+                    </td>
+                </tr>
+                </tbody>
+<?php
             }
-            ?>
-
-        </form>
-
+?>
     </div>
 </table>
+<?php
+for ($page = 1;$page <= $page_cnt; $page++) {
+    $url = sprintf("%s?pageNum=%s",
+        $_SERVER['PHP_SELF'],
+        $page
+    );
+    ?>
+    <a href='<?=$url?>'>[<?=$page?>]</a>
+<?php
+}
+?>
 </body>
 </html>
