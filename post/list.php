b6323ce699ac677ee78f44223fe13e27d5908ec2
young0264 <ny2485@naver.com>
2023-01-05 17:49:44 +0900

    Add : 파일 업로드 기능

--- post/list.php
+++ post/list.php
@@ -1,3 +1,10 @@
+<?php
+/**
+ * 게시글 리스트
+ * 남의영 (2022-01-05)
+ * 게시글들을 보여주는 페이지 입니다
+ */
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,11 +13,13 @@
 <body>
 <script type="text/javascript">
     function delete_id(id) {
-        location.replace("../post/delete.php?postId=" + id);}
+        location.replace("../post/delete.php?postId=" + id);
+    }
 </script>
 <script>
-    function post_modify(id){
-        location.replace("../post/update.php?postId=" + id);}
+    function post_modify(id) {
+        location.replace("../post/update.php?postId=" + id);
+    }
 </script>
 
 <div class=top><h2>게시판</h2></div>
@@ -22,7 +31,8 @@
             <option value="title">제목</option>
             <option value="content">내용</option>
         </select>
-        <input type="text" name="search" size="40" required="required" /> <button>검색</button>
+        <input type="text" name="search" size="40" required="required"/>
+        <button>검색</button>
     </form>
 </div>
 <table class="middle">
@@ -35,6 +45,7 @@
         <th>작성일</th>
         <th>수정</th>
         <th>삭제</th>
+        <th>첨부파일</th>
     </tr>
     </thead>
 
@@ -64,10 +75,13 @@
                 <td><?php echo $row['created']; ?></td>
                 <!--                    onclick을 통한 삭제와 form submit을 통한 삭제 -->
                 <td>
-                    <button type="button" onclick="post_modify('<?=$row['id']?>')">수정</button>
+                    <button type="button" onclick="post_modify('<?= $row['id'] ?>')">수정</button>
                 </td>
                 <td>
                     <button type="button" onclick="delete_id('<?= $row['id'] ?>')">삭제</button>
+                </td>
+                <td>
+                    <a href="../upload/<?= $row['file'] ?>" download><?= $row['file'] ?></a>
                 </td>
             </tr>
             </tbody>
@@ -78,17 +92,18 @@
 </table>
 <div class="pageNum">
 
-<?php
-for ($page = 1; $page <= $page_cnt; $page++) {
-//    echo $_SERVER['PHP_SELF']; //untitled/post/list
+    <?php
+    for ($page = 1;
+    $page <= $page_cnt;
+    $page++) {
     $url = sprintf("%s?pageNum=%s",
         $_SERVER['PHP_SELF'],
         $page);
     ?>
-            <a href='<?= $url ?>'>[<?= $page ?>]
-<?php
-                }
-?>
+    <a href='<?= $url ?>'>[<?= $page ?>]
+        <?php
+        }
+        ?>
 </div>
 </body>
 </html>
--- post/write.php
+++ post/write.php
@@ -3,7 +3,6 @@ date_default_timezone_set('Asia/Seoul');
 $connect = mysqli_connect("localhost", "root", "7pifz9!!", "loginexam") or die("fail");
 
 
-
 $username = $_COOKIE['id'];
 $title = $_POST['title'];
 $content = $_POST['content'];
@@ -12,66 +11,29 @@ $date = date('Y-m-d H:i:s');
 echo "<h2>$title</h2>";
 echo "<h2>$content</h2>";
 echo "<h2>$date</h2>";
+$tmp_file = $_FILES['SelectFile']['tmp_name']; //파일
+$origin_name = $_FILES['SelectFile']['name'];   //파일의 이름
 
-$upload_dir = 'uploads/';
-$upload_file = $upload_dir . $_FILES['SelectFile']['name'];
-
-$file_name = iconv("utf-8","CP949",$upload_file);
-//echo "<script>
-//  alert('$tmpfile');
-//  </script>";
-//print "<pre>";
-//if (move_uploaded_file($_FILES['SelectFile']['tmp_name'], $file_name)) {
-//    print "[수신한 내용]<br><br>";
-//    print "PATH: " .$upload_file."<br>";
-//    print "제목 : ".$_POST['title']."<br>";
-//    print "내용 : ".$_POST['content']."<br>";
-//    print "파일 :".$_FILES['SelectFile']['type']."<br>";
-//    if($_FILES['SelectFile']['type']=="image/jpeg"||$_FILES['SelectFile']['type']=="image/gif"){
-//        print "<img src=$upload_file width='300'>";
-//    }
-//}
-//else {
-//    print "파일 업로드  실패 : ";
-//    switch ($_FILES[SelectFile][error]) {
-//        case UPLOAD_ERR_INI_SIZE:
-//            print "php.ini 파일의 upload_max_filesize(".ini_get("upload_max_filesize").")보다 큽니다.<br>";
-//            break;
-//        case UPLOAD_ERR_FORM_SIZE:
-//            print "업로드 한 파일이 Form의 MAX_FILE_SIZE 값보다 큽니다.<br>";
-//            break;
-//        case UPLOAD_ERR_PARTIAL:
-//            print "파일의 일부분만 전송되었습니다.<br>";
-//            break;
-//        case UPLOAD_ERR_NO_FILE:
-//            print "파일이 전송되지 않았습니다.<br>";
-//            break;
-//        case UPLOAD_ERR_NO_TMP_DIR:
-//            print "임시 디렉토리가 없습니다.<br>";
-//            break;
-//    }
-//    print_r($_FILES);
-//}
-//print "</pre>";
-
-if($username && $title && $content){
-    echo "<h2>11111111</h2>";
-
-    $query = "insert into post (title, content, created, nickname ) values ('$title', '$content', '$date' ,'$username')";
+$file_name = iconv("utf-8", "EUC-KR", $_FILES['SelectFile']['name']);
+$folder = "../upload/".$file_name;
+move_uploaded_file($tmp_file, $folder);
+if ($username && $title && $content) {
+    $query = "insert into post (title, content, created, nickname, file ) values ('$title', '$content', '$date' ,'$username','$origin_name')";
     echo "<h2>$query</h2>";
-    $result = mysqli_query($connect, $query);
-
+    mysqli_query($connect, $query);
     ?>
-        "<script>
+    "
+    <script>
         alert('글쓰기 완료되었습니다.');
         location.replace("/untitled/post/list.php");
-</script>";
-<?php
+    </script>";
+    <?php
 
-}
-else{
+} else {
     echo "<script>
       alert('글쓰기에 실패했습니다.');
       history.back();</script>";
+
 }
 ?>
+
