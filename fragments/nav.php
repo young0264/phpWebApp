b6f542f2aa8cdd371a841bf7fffb39ee39124a44
young0264 <ny2485@naver.com>
2023-01-06 16:43:54 +0900

    Add : page navigation bar 추가

--- fragments/nav.php
+++ fragments/nav.php
@@ -1,1 +1,9 @@
-<?php
+<nav  class="navbar navbar-expand-sm navbar-dark bg-dark">
+    <a class="navbar-brand" href="/" >
+        <img src="../images/logo.png" width="100" height="60">
+    </a>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+    </ul>
+</div>
+</nav>
