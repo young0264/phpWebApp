af0e3c1cf23226d440aaccdd47dbe82b5bb4fa90
young0264 <ny2485@naver.com>
2023-01-06 16:43:17 +0900

    Add : 댓글기능추가

--- comment/comment.php
+++ comment/comment.php
@@ -1,1 +1,23 @@
 <?php
+require_once("../fragments/header.html");
+$connect = mysqli_connect("localhost", "root", "7pifz9!!", "loginexam") or die("fail");
+$postId = $_POST['postId'];
+$nickname = $_POST['nickname'];
+$content = $_POST['content'];
+$created = $_POST['created'];
+
+if ($postId && $_POST['content']) {
+    $query = "insert into comment(nickname, content, created, post_id) 
+                            values('$nickname','$content', '$created', '$postId')";
+    mysqli_query($connect, $query);
+    ?>
+    <script>
+        alert('댓글이 등록되었습니다.');
+        location.replace("../post/detail.php?postId=<?=$postId?>");
+    </script>
+    <?php
+} else {
+    echo "<script>
+            alert('댓글 작성에 실패했습니다.')
+            history.back()</script>";}
+?>
