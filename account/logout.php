5448be106489cdcd6d2ad4990bd67d1c55607800
young0264 <ny2485@naver.com>
2023-01-12 16:26:36 +0900

    Bug : 로그아웃시 세션유지되는 버그 fix

--- account/logout.php
+++ account/logout.php
@@ -1,1 +1,22 @@
 <?php
+$nickname = $_COOKIE['id'];
+
+setcookie('id', '', time() - 3600, '/');
+if (isset($_COOKIE['id'])) {
+    ?>
+    <script>
+        alert("로그아웃 되었습니다.");
+        location.replace("../main/mainpage.html");
+    </script>
+    <?php } else { ?>
+        <script>
+            alert("아이디 혹은 비밀번호가 잘못되었습니다.");
+            history.back();
+        </script>
+        <?php } ?>
+    <?php  ?>
+
+
+
+
+
