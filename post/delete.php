f9ab6937f9ab643430c5cc2e7d348367d9c0e07f
young0264 <ny2485@naver.com>
2023-01-26 18:50:17 +0900

    Add: 23-01-26

--- post/delete.php
+++ post/delete.php
@@ -6,10 +6,22 @@
     }
 
     $postId = $_GET['postId'];
+    $postsId = $_POST['post_id'];
+
+if ($postsId == null) {
     $query = "delete from post where id='$postId'";
     if (!mysqli_query($connect, $query)) {
         alert("값이 삭제되지 않았습니다");
     }
+}else {
+    foreach ($postsId as $post) {
+        $query = "delete from post where id='$post'";
+        if (!mysqli_query($connect, $query)) {
+            alert("값이 삭제되지 않았습니다");
+        }
+    }
+}
+
 
     alert(sprintf("%d 번째 값이 삭제되었습니다.", $postId));
 ?>
--- post/list.php
+++ post/list.php
@@ -31,12 +31,35 @@ $result = mysqli_query($connect, $sql);
  * 게시글들을 보여주는 페이지 입니다
  */
 
+
 ?>
 <script type="text/javascript">
     function go(url, id) {
-        let parameter = (id !== "" && id !== undefined) ? "?postId=" + id : "";
-        location.href = url + parameter;
+        if (id != null) {
+            let parameter = (id !== "" && id !== undefined) ? "?postId=" + id : "";
+            location.href = url + parameter;
+        } else {
+            alert("go posts list");
+            var data1 = $("#post_list").val();
+            alert(data1);
+            console.log(data1);
+            // $.ajax({
+            //     type: 'POST',
+            //     url: 'delete.php',
+            //     data: $("#post_list").serialize(),
+            //     dataType: "json",
+            //     success: function (args) {
+            //         alert("ajax post delete success");
+            //     }
+            //     error: function (e) {
+            //         alert(e.responseText);
+            //     }
+            // })
+        }
     }
+
+
+
 </script>
 <body class="container" style="background-color: aliceblue">
 <div class="container" style="background-color: aliceblue">
@@ -46,82 +69,100 @@ $result = mysqli_query($connect, $sql);
                 <h2>게시판</h2>
             </div>
             <div id="search_box">
-                <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+                <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
                     <select class="btn btn-outline-info" name="category">
                         <option value="nickname">작성자</option>
                         <option value="title">제목</option>
                         <option value="content">내용</option>
                     </select>
-                    <input class="btn btn-outline-info" type="text" name="search" size="40" value="<?=$search?>" />
+                    <input class="btn btn-outline-info" type="text" name="search" size="40" value="<?= $search ?>"/>
                     <button type="button" class="btn btn-outline-info">검색</button>
-                    <button type="button" class="btn btn-outline-info" onclick="go('write.html')" style="float:right;">글쓰기</button>
+                    <button type="button" class="btn btn-outline-info" onclick="go('delete.php', null)" style="float:right;">선택삭제
+                    </button>
+                    <button type="button" class="btn btn-outline-info" onclick="go('write.html')" style="float:right;">
+                        글쓰기
+                    </button>
                     <button type="button" class="btn btn-outline-info"
                             onclick="go('../file/my_post_down.php')"
                             style="float:right;">엑셀다운로드
                     </button>
                 </form>
             </div>
-            <table class="table table-bordered table-hover vw-50 vh-50" style="padding-left: 100px;">
-                <tr>
-                    <th>Post ID</th>
-                    <th>제목</th>
-                    <th>내용</th>
-                    <th>작성자</th>
-                    <th>작성일</th>
-                    <th>수정/삭제</th>
-                    <th>첨부파일</th>
-                </tr>
-                <tbody>
-<?php
-                while ($row = mysqli_fetch_array($result)) {
-                    $sql3 = "select * from file 
-                            where idx = ".$row['id']."
-                                order by id";
-
-                    $res_file = mysqli_query($connect, $sql3);
-                    $file_str = "";
-                    while ($row_file = mysqli_fetch_array($res_file)) {
-                        $file_str .= $file_str === ""
-                                        ? "<a href='../upload/{$row_file['name']}' style='color:darkblue'>{$row_file['name']}</a>"
-                                        : ","."<a href='../upload/{$row_file['name']}' style='color:darkblue'>{$row_file['name']}</a>" ;
-                    }
-?>
+            <form id="post_list" method="post">
+                <table class="table table-bordered table-hover vw-50 vh-50" style="padding-left: 100px;">
                     <tr>
-                        <td><?= $row['id'] ?></td>
-<!--                        <td><a href="../post/detail.php?postId=--><?php //= $row['id'] ?><!--" style="color: darkblue">--><?php //= $row['title'], "(", $row['parent_cnt'], ")" ?><!--</a></td>-->
-
-                        <td id="accordion">
-                            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#<?=$row['id']?>" aria-expanded="false" aria-controls="collapseTwo">
-                                <?= $row['title'], "(", $row['parent_cnt'], ")" ?>
-                            </button>
-                            <div id="<?=$row['id']?>" class="collapse" aria-labelledby="headingTwo" data-parent="accordion">
-                                <?php
-                                $comment_sql = "select content from post where idx=".$row['id']." order by created ";
-                                $comments = mysqli_query($connect, $comment_sql);
-                                foreach ($comments as $comment) {?>
-                                    <h6> <?=$comment['content']?> </h6>
-                                <?php } ?>
-
-                            </div>
-                        </td>
-
-                        <td><?=$row['content'] ?></td>
-                        <td><?=$row['nickname']; ?></td>
-                        <td><?=$row['created']; ?></td>
-                        <!--                    onclick을 통한 삭제와 form submit을 통한 삭제 -->
-
-                        <td>
-                            <button class="btn btn-outline-dark" type="button" onclick="go('detail.php', '<?=$row['id']?>')">상세보기</button>
-                            <?php if ($row['nickname']==$login_user) { ?>
-                                <button class="btn btn-outline-dark" type="button" onclick="go('update.php', '<?=$row['id'] ?>')">수정</button>
-                                <button class="btn btn-outline-dark" type="button" onclick="go('delete.php', '<?=$row['id']?>')">삭제</button>
-                            <?php } ?>
-                        </td>
-                        <td><?=$file_str?></td>
+                        <th>선택</th>
+                        <th>Post ID</th>
+                        <th>제목</th>
+                        <th>내용</th>
+                        <th>작성자</th>
+                        <th>작성일</th>
+                        <th>수정/삭제</th>
+                        <th>첨부파일</th>
                     </tr>
-                <?php } ?>
-                </tbody>
-            </table>
+                    <tbody>
+                    <?php while ($row = mysqli_fetch_array($result)) {
+                        $sql3 = "select * from file 
+                                where idx = " . $row['id'] . "
+                                    order by id";
+
+                        $res_file = mysqli_query($connect, $sql3);
+                        $file_str = "";
+                        while ($row_file = mysqli_fetch_array($res_file)) {
+                            $file_str .= $file_str === ""
+                                ? "<a href='../upload/{$row_file['name']}' style='color:darkblue'>{$row_file['name']}</a>"
+                                : "," . "<a href='../upload/{$row_file['name']}' style='color:darkblue'>{$row_file['name']}</a>";
+                        } ?>
+                        <tr>
+                            <td><input type="checkbox" name="post_id[]" value="<?= $row['id'] ?>"></td>
+                            <td><?= $row['id'] ?></td>
+                            <!--                        <td><a href="../post/detail.php?postId=-->
+                            <?php //= $row['id'] ?><!--" style="color: darkblue">-->
+                            <?php //= $row['title'], "(", $row['parent_cnt'], ")" ?><!--</a></td>-->
+                            <td id="accordion">
+                                <button class="btn btn-link collapsed" data-toggle="collapse"
+                                        data-target="#<?= $row['id'] ?>" aria-expanded="false"
+                                        aria-controls="collapseTwo">
+                                    <?= $row['title'], "(", $row['parent_cnt'], ")" ?>
+                                </button>
+                                <div id="<?= $row['id'] ?>" class="collapse" aria-labelledby="headingTwo"
+                                     data-parent="accordion">
+                                    <?php
+                                    $comment_sql = "select content from post where idx=" . $row['id'] . " order by created";
+                                    $comments = mysqli_query($connect, $comment_sql);
+                                    ?>
+                                    <ul><?php
+                                        foreach ($comments as $comment) { ?>
+                                            <li> <?= $comment['content'] ?></li>
+                                        <?php } ?>
+                                    </ul>
+                                </div>
+                            </td>
+
+                            <td><?= $row['content'] ?></td>
+                            <td><?= $row['nickname']; ?></td>
+                            <td><?= $row['created']; ?></td>
+                            <!--   onclick을 통한 삭제와 form submit을 통한 삭제 -->
+
+                            <td>
+                                <button class="btn btn-outline-dark" type="button"
+                                        onclick="go('detail.php', '<?= $row['id'] ?>')">상세보기
+                                </button>
+                                <?php if ($row['nickname'] == $login_user) { ?>
+                                    <button class="btn btn-outline-dark" type="button"
+                                            onclick="go('update.php', '<?= $row['id'] ?>')">수정
+                                    </button>
+                                    <button class="btn btn-outline-dark" type="button"
+                                            onclick="go('delete.php', '<?= $row['id'] ?>')">삭제
+                                    </button>
+                                <?php } ?>
+                            </td>
+                            <td><?= $file_str ?></td>
+                        </tr>
+                    <?php } ?>
+                    </tbody>
+                </table>
+            </form>
             <nav class="pageNum">
                 <nav aria-label="Page navigation example">
                     <ul class="pagination">
